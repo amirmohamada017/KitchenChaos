8289f840201bb94cb61b11be6d455f5cb6a61641
Amirmohamad A <amirmohamada017@gmail.com>
2023-11-07 09:38:11 +0330

    add character visual and implement rotation.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -22,5 +22,8 @@ public class Player : MonoBehaviour
 
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
         transform.position += moveDir * (Time.deltaTime * moveSpeed);
+
+        var rotateSpeed = 10f;
+        transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
     }
 }
