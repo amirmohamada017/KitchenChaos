b042e905590261f760d6866df67e9a1afce002c5
Amirmohamad A <amirmohamada017@gmail.com>
2023-11-07 13:15:43 +0330

    handle collisions.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -8,10 +8,25 @@ public class Player : MonoBehaviour
     
     private void Update()
     {
-        var inputVector = gameInput.GetMovementVectorNormalized();
+        Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        transform.position += moveDir * (Time.deltaTime * moveSpeed);
-
+        var moveDirX = new Vector3(inputVector.x, 0, 0);
+        var moveDirZ = new Vector3(0, 0, inputVector.y);
+        
+        float moveDistance = moveSpeed * Time.deltaTime;
+        float playerHeight = 2f;
+        float playerRadius = .7f;
+        
+        bool canMoveX = !Physics.CapsuleCast(transform.position,
+            (transform.position + Vector3.up * playerHeight), playerRadius, moveDirX, moveDistance);
+        bool canMoveZ = !Physics.CapsuleCast(transform.position,
+            (transform.position + Vector3.up * playerHeight), playerRadius, moveDirZ, moveDistance);
+        
+        if (canMoveX)
+            transform.position += moveDirX * moveDistance;
+        if (canMoveZ)
+            transform.position += moveDirZ * moveDistance;
+        
         var rotateSpeed = 10f;
         transform.forward = Vector3.Slerp(transform.forward, moveDir, Time.deltaTime * rotateSpeed);
 
