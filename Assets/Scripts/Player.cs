8299f66f5b5c3d72cfae290bb459380b3bd6628e
Amirmohamad A <amirmohamada017@gmail.com>
2023-11-12 10:38:31 +0330

    handle interactions.

--- Assets/Scripts/Player.cs
+++ Assets/Scripts/Player.cs
@@ -1,17 +1,55 @@
+using System;
 using UnityEngine;
 
 public class Player : MonoBehaviour
 {
+    public static Player Instance { private set; get; }
+    public event EventHandler<OnSelectedCounterChangedEventArgs> OnSelectedCounterChanged;
+
+    public class OnSelectedCounterChangedEventArgs : EventArgs
+    {
+        public ClearCounter selectedCounter;
+    }
+    
     [SerializeField] private GameInput gameInput;
     [SerializeField] private float moveSpeed = 7f;
-    private bool _isWalking;
+    [SerializeField] private LayerMask countersLayerMask;
     
+    private bool _isWalking;
+    private Vector3 _lastInteractDir;
+    private ClearCounter _selectedCounter;
+
+    private void Awake()
+    {
+        if (Instance != null)
+            Debug.LogError("there is more than one player instance!");
+        
+        Instance = this;
+    }
+
+    private void Start()
+    {
+        gameInput.OnInteractAction += GameInput_OnInteractAction;
+    }
+
+    private void GameInput_OnInteractAction(object sender, System.EventArgs e)
+    {
+        if (_selectedCounter != null)
+            _selectedCounter.Interact();
+    }
+
     private void Update()
     {
         Vector2 inputVector = gameInput.GetMovementVectorNormalized();
         var moveDir = new Vector3(inputVector.x, 0, inputVector.y);
-        var moveDirX = new Vector3(inputVector.x, 0, 0);
-        var moveDirZ = new Vector3(0, 0, inputVector.y);
+        HandleMovement(moveDir);
+        HandleInteractions(moveDir);
+    }
+
+    private void HandleMovement(Vector3 moveDir)
+    {
+        var moveDirX = new Vector3(moveDir.x, 0, 0);
+        var moveDirZ = new Vector3(0, 0, moveDir.z);
         
         float moveDistance = moveSpeed * Time.deltaTime;
         float playerHeight = 2f;
@@ -33,8 +71,39 @@ public class Player : MonoBehaviour
         _isWalking = moveDir != Vector3.zero;
     }
 
+    private void HandleInteractions(Vector3 moveDir)
+    {
+        if (moveDir != Vector3.zero)
+            _lastInteractDir = moveDir;
+        
+        var interactDistance = 2f;
+        if (Physics.Raycast(transform.position, _lastInteractDir, out RaycastHit raycastHit,
+                interactDistance, countersLayerMask))
+        {
+            if (raycastHit.transform.TryGetComponent(out ClearCounter clearCounter))
+            {
+                if (clearCounter != null && clearCounter != _selectedCounter)
+                    SetSelectedCounter(clearCounter);
+            }
+            else
+                SetSelectedCounter(null);
+        }
+        else
+            SetSelectedCounter(null);
+    }
+
     public bool Is_Walking()
     {
         return _isWalking;
+    }
+
+    private void SetSelectedCounter(ClearCounter selectedCounter)
+    {
+        _selectedCounter = selectedCounter;
+        
+        OnSelectedCounterChanged?.Invoke(this, new OnSelectedCounterChangedEventArgs
+        {
+            selectedCounter = _selectedCounter
+        });
     }
 }
---
+++ Assets/Scripts/SelectedCounterVisual.cs
@@ -0,0 +1,31 @@
+using System;
+using UnityEngine;
+
+public class SelectedCounterVisual : MonoBehaviour
+{
+    [SerializeField] private ClearCounter _clearCounter;
+    [SerializeField] private GameObject visualGameObject;
+    private void Start()
+    {
+        Player.Instance.OnSelectedCounterChanged += Player_OnSelectedCounterChanged;
+    }
+
+    private void Player_OnSelectedCounterChanged(object sender, Player.OnSelectedCounterChangedEventArgs e)
+    {
+        if (e.selectedCounter == _clearCounter)
+            Show();
+        else
+            Hide();
+        
+    }
+
+    private void Show()
+    {
+        visualGameObject.SetActive(true);
+    }
+    
+    private void Hide()
+    {
+        visualGameObject.SetActive(false);
+    }
+}
